4e99e9ea21092813ff7a32022b077605ee67d6eb
Rasto <rastouhrin1@gmail.com>
2023-01-15 15:14:05 +0100

    fixed 7s for 3 players

--- lib/main.dart
+++ lib/main.dart
@@ -16,17 +16,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
+      create: (context) => AppState(),
       child: MaterialApp(
         title: _title,
-        home: MyStatefulWidget(),
+        home: AppBase(),
         debugShowCheckedModeBanner: false,
       ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
+class AppState extends ChangeNotifier {
   var history = [];
   int randomValue = 0;
   int mainIndex = 0;
@@ -41,15 +41,15 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
+class AppBase extends StatefulWidget {
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<AppBase> createState() => AppBaseState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class AppBaseState extends State<AppBase> {
   int _selectedIndex = 0;
   List<Widget> _widgetOptions = <Widget>[
-    MyWidget(),
+    HomePage(),
     HistoryPage(),
   ];
   void _onItemTapped(int index) {
@@ -60,7 +60,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    var appState = context.watch<AppState>();
 
     showAlertDialog(BuildContext context) {
       // Create button
@@ -152,122 +152,110 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   }
 }
 
-class MyWidget extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
-  var order = [1, 2, 3, 1, 2, 4, 1, 3, 5, 1, 2, 4, 1, 2, 3];
-
+class _HomePageState extends State<HomePage> {
+  var halfOrder = [1, 2, 3, 1, 2, 4, 1, 3, 5, 1, 2, 4, 1, 2, 3];
+  var order = [];
+  var newOrder = [];
   var rng = Random();
-
+  _HomePageState() {
+    order = [...halfOrder, ...halfOrder];
+  }
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var sevens = [];
-    var players = 4;
+    var appState = context.watch<AppState>();
+    // var sevens = [];
+    // var players = 3;
 
     void generateNextNumber(int first, int second) {
       if (appState.mainIndex == 0) {
-        sevens = [];
-        if (players == 3) {
-          print('nextRoundSevens 1 -  ${appState.nextRoundSevens}');
-          sevens = [...appState.nextRoundSevens];
-          print('sevens $sevens');
-          var pool = [1, 2, 3];
-          var randomN = rng.nextInt(pool.length);
-          sevens.add(pool[randomN]);
-          pool.removeAt(randomN);
+        order = [...halfOrder, ...halfOrder];
+        var sevensPoolForThree = [1, 1, 2, 2, 3, 3];
+        sevensPoolForThree.shuffle();
+        print('sevensPoolForThree $sevensPoolForThree');
 
-          randomN = rng.nextInt(pool.length);
-          sevens.add(pool[randomN]);
-          pool.removeAt(randomN);
+        // sevens = [];
+        // var pool = [];
+        var sevenCyclePositions = <int>[];
+        var tempSeven = 0;
 
-          sevens.add(pool[0]);
+        while (sevenCyclePositions.length < 6) {
+          tempSeven = rng.nextInt(12) + 1;
 
-          if (sevens.length < 5) {
-            pool = [1, 2, 3];
-            randomN = rng.nextInt(pool.length);
-            sevens.add(pool[randomN]);
-            pool.removeAt(randomN);
-            if (sevens.length == 4) {
-              randomN = rng.nextInt(pool.length);
-              sevens.add(pool[randomN]);
-              pool.removeAt(randomN);
-            }
+          if (!sevenCyclePositions.contains(tempSeven)) {
+            sevenCyclePositions.add(tempSeven);
           }
-
-          appState.nextRoundSevens = [...pool];
-          print('appState.nextRoundSevens ${appState.nextRoundSevens}');
-
-          pool = [1, 2, 3];
         }
-        if (players == 4) {
-          // pri hre styroch
-          var pool = [1, 2, 3, 4];
-          if (appState.nextRoundSevens.isEmpty) {
-            sevens = [...pool];
-            sevens.shuffle();
-          } else {
-            if (appState.nextRoundSevens.length == 1) {
-              sevens = [...appState.nextRoundSevens, ...pool];
-            } else {
-              sevens = [...appState.nextRoundSevens];
-            }
-            sevens.shuffle();
+        sevenCyclePositions.sort();
+        print('sevenCyclePositions $sevenCyclePositions');
+
+        var cyklus = 1;
+        var position = 1;
+        var sevensIndex = 0;
+
+        for (int i = 0; i < 36; i++) {
+          print('order $order');
+          // print('voslo ${sevenCyclePositions[sevensIndex]}');
+
+          if (sevenCyclePositions.length > sevensIndex &&
+              cyklus == sevenCyclePositions[sevensIndex] &&
+              position == sevensPoolForThree[sevensIndex]) {
+            print('VOSLO');
+
+            order.insert(i, 7);
+            sevensIndex++;
           }
-          print("waaat sevens $sevens");
+          position++;
 
-          var randomN = 0;
-          for (var i = 1; i <= 3; i++) {
-            if (sevens.length == 5) break;
-            randomN = rng.nextInt(pool.length);
-            sevens.add(pool[randomN]);
-            pool.removeAt(randomN);
+          if (position == 4) {
+            position = 1;
+            cyklus++;
           }
-          // if (sevens.length < 5) {
-          //   randomN = rng.nextInt(pool.length);
-          //   sevens.add(pool[randomN]);
-          //   pool.removeAt(randomN);
-          //   if (sevens.length < 5) {
-          //     randomN = rng.nextInt(pool.length);
-          //     sevens.add(pool[randomN]);
-          //     pool.removeAt(randomN);
-          //   }
-          //   if (sevens.length < 5) {
-          //     randomN = rng.nextInt(pool.length);
-          //     sevens.add(pool[randomN]);
-          //     pool.removeAt(randomN);
-          //   }
-          // }
-
-          // randomN = rng.nextInt(pool.length);
-          // sevens.add(pool[randomN]);
-          // pool.removeAt(randomN);
-
-          // randomN = rng.nextInt(pool.length);
-          // sevens.add(pool[randomN]);
-          // pool.removeAt(randomN);
-
-          // sevens.add(pool[0]);
-
-          // pool = [1, 2, 3, 4];
-          // randomN = rng.nextInt(pool.length);
-          // sevens.add(pool[randomN]);
-          appState.nextRoundSevens = [...pool];
         }
-        order = [1, 2, 3, 1, 2, 4, 1, 3, 5, 1, 2, 4, 1, 2, 3];
+        print('order: $order');
+        print('WAT: ${order.where((element) => element == 7).length}');
+        print('WAT sum: ${order.length}');
 
-        print("waaat sevens $sevens");
-        order.insert(sevens[0] - 1, 7);
-        order.insert(sevens[1] + 3, 7);
-        order.insert(sevens[2] + 7, 7);
-        order.insert(sevens[3] + 11, 7);
-        order.insert(sevens[4] + 15, 7);
+        // if (players == 3) pool = [1, 2, 3];
+        // if (players == 4) pool = [1, 2, 3, 4];
 
-        print('WOW $order');
+        // if (appState.nextRoundSevens.isEmpty) {
+        //   sevens = [...pool];
+        //   sevens.shuffle();
+        // } else {
+        //   if (appState.nextRoundSevens.length == 1) {
+        //     sevens = [...appState.nextRoundSevens, ...pool];
+        //   } else {
+        //     sevens = [...appState.nextRoundSevens];
+        //   }
+        //   sevens.shuffle();
+        // }
+        // print("waaat sevens $sevens");
+
+        // var randomN = 0;
+        // for (var i = 1; i <= 3; i++) {
+        //   if (sevens.length == 5) break;
+        //   randomN = rng.nextInt(pool.length);
+        //   sevens.add(pool[randomN]);
+        //   pool.removeAt(randomN);
+        // }
+        // appState.nextRoundSevens = [...pool];
+        // order = [1, 2, 3, 1, 2, 4, 1, 3, 5, 1, 2, 4, 1, 2, 3];
+
+        // print("waaat sevens $sevens");
+        // order.insert(sevens[0] - 1, 7);
+        // order.insert(sevens[1] + 3, 7);
+        // order.insert(sevens[2] + 7, 7);
+        // order.insert(sevens[3] + 11, 7);
+        // order.insert(sevens[4] + 15, 7);
+
+        // print('WOW $order');
       }
+
       var tempRandomValue = 0;
       var countOfFirst = appState.history.where((x) => x == first).length;
       var countOfSecond = appState.history.where((x) => x == second).length;
@@ -284,7 +272,7 @@ class _MyWidgetState extends State<MyWidget> {
         appState.history.add(tempRandomValue);
         appState.randomValue = tempRandomValue;
         appState.mainIndex++;
-        if (appState.mainIndex == 20) appState.mainIndex = 0;
+        if (appState.mainIndex == 36) appState.mainIndex = 0;
       });
     }
 
@@ -357,7 +345,7 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    var appState = context.watch<AppState>();
     // int acc = 0;
     // var a = appState.history.asMap().entries.map((e) {
     //   var idx = e.key;
@@ -372,7 +360,7 @@ class _HistoryPageState extends State<HistoryPage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(20),
-            child: Text('Kockou sa uz hodilo ${appState.history.length}x:'),
+            child: Text('Kockou sa uz hodilo ${appState.history.length} krat:'),
           ),
           Text('${appState.history}'),
         ],
