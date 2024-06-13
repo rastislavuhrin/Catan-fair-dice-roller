c9562f3ed4fbb1493015a5cf69324848b3ca426a
Rasto <rastouhrin1@gmail.com>
2024-06-13 09:18:04 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -324,7 +324,7 @@ class _HomePageState extends State<HomePage> {
       }
       // print('order $order');
       // print('sevenCyclePositions $sevenCyclePositions');
-      // print('apspState.whichPlayerRollsseven ${appStat.whichPlayerRollsSeven}');
+      // print('apspSt.whichPlayerRollsseven ${appStat.whichPlayerRollsSeven}');
     }
 
     void generateNextNumber(int first, int second) {
