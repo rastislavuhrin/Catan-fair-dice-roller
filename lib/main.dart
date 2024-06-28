aeffc35b95b4b1066a0978babccd17d5445ad43e
Rasto <rastouhrin1@gmail.com>
2024-06-28 22:32:16 +0200

    fixed main position

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 'Flutter Code Ssssssllllssrrssample';
+  static const String _title = 'Flutter Cossde Ssssssllllssrrssample';
 
   @override
   Widget build(BuildContext context) {
