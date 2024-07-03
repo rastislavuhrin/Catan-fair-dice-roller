41193ad6febfbbd2ab67464dc44c3e92713bd7d6
Rasto <rastouhrin1@gmail.com>
2024-07-03 13:57:10 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   static const String _title =
-      'Flsssutsssstessr Cosssssde Ssssssllllssrrssample';
+      'Flsssutsssstessr Cosssssssde Ssssssllllssrrssample';
 
   @override
   Widget build(BuildContext context) {
