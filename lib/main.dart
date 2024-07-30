ea9a3f07fccd5e14767f852929727e9fdcfb1d24
Rasto <rastouhrin1@gmail.com>
2024-07-30 15:30:05 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const String _title = 's sggggdgsshhhhhssssssssssss s';
+  static const String _title = 's sggggdgnnnnnnsssshhhhhssssssssssss s';
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
